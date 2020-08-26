c8261e7121fd610eec47277817b16a4e2eb4321b
Joel Panocca <panocca@hotmail.com>
2020-08-25 19:58:35 -0500

    1era migración

--- Dominio/Curso.cs
+++ Dominio/Curso.cs
@@ -1,9 +1,10 @@
+using System;
 using System.Collections.Generic; 
 namespace Dominio
 {
     public class Curso
     {
-        public int CursoId { get; set; }    
+        public Guid CursoId { get; set; }    
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public System.DateTime? FechaPublicacion { get; set; }
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Persistencia;
 
 namespace WebAPI
 {
@@ -13,7 +16,22 @@ namespace WebAPI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var hostServer = CreateHostBuilder(args).Build();
+            using (var ambiente = hostServer.Services.CreateScope())
+            {
+                var services = ambiente.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<CursosOnlineContext>();
+                    context.Database.Migrate();
+                }
+                catch (System.Exception e)
+                {
+                    var logging = services.GetRequiredService<ILogger<Program>>();
+                    logging.LogError(e, "Ocurrió un error en la migración");
+                }
+            }
+            hostServer.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
